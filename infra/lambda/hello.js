29967e95fbf93f365ad4e3ce47ade96c37773246
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-11-16 01:13:17 +0900

    watch Lambda function code

--- infra/lambda/hello.js
+++ infra/lambda/hello.js
@@ -6,6 +6,6 @@ exports.handler = async function(event) {
     headers: {
       'Content-Type': 'text/plain',
     },
-    body: `Hello, CDK! You've hit ${path}\n`,
+    body: `Good Morning! You've hit ${path}\n`,
   }
 }
